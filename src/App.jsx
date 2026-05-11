0e710ebfdca52c36b9cfae0cff9e58a1d2ec7d63
Pedro Alex <pedro.alex@ifpi.edu.br>
2026-05-11 16:05:00 -0300

    Exemplo realizado na aula 12.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MeuComponente from './components/MeuComponente'
+import './index.css'
+
+function App() {
+  return (
+    <MeuComponente />
+  )
+}
+
+export default App
---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MeuComponente(){
+    return (
+        <h1>Olá mundo! Primeira aula de React.</h1>
+    )
+}
+
+export default MeuComponente;
